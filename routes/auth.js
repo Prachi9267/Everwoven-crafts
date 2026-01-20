39930430edd24ad24d842e6cca33d90fa4d5b94d
Prachi9267 <prachi.gupta9267@gmail.com>
2026-01-20 21:56:49 +0530

    New modifications

---
+++ routes/auth.js
@@ -0,0 +1,59 @@
+import express from "express";
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+const router = express.Router();
+
+// REGISTER
+router.post("/register", async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+
+        const existing = await User.findOne({ email });
+        if (existing) {
+            return res.status(400).json({ message: "Email already exists" });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = new User({ name, email, password: hashedPassword });
+        await user.save();
+
+        res.status(201).json({ message: "Account created successfully!" });
+
+    } catch (error) {
+        res.status(500).json({ message: "Server error" });
+    }
+});
+
+// LOGIN
+router.post("/login", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+        if (!user) return res.status(400).json({ message: "User not found" });
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) return res.status(400).json({ message: "Incorrect password" });
+
+        const token = jwt.sign({ id: user._id },
+            process.env.JWT_SECRET,
+            { expiresIn: "7d" }
+        );
+
+        res.json({
+            message: "Login successful",
+            token,
+            user: { name: user.name, email: user.email }
+        });
+
+    } catch (error) {
+        res.status(500).json({ message: "Server error" });
+    }
+});
+
+export default router;
