44b7572a6fbe5d98e1d142bb88305ac72214c210
Prachi9267 <prachi.gupta9267@gmail.com>
2026-02-18 16:49:32 +0530

    fixed frontend connectivity issue

--- backend/server.js
+++ backend/server.js
@@ -61,4 +61,7 @@ app.use("/api/orders", orderRoutes);
 // -------------------
 // 5. Start Server
 // -------------------
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
